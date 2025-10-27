844cf7c3e8c582338a8c9d70ffc066ac525f2a09
Erling Sigurdson <efimov-d-v@yandex.ru>
2025-10-27 08:15:24 +0300

    Update README and comments re binary literals (binary number notation).

--- src/SegMap595.cpp
+++ src/SegMap595.cpp
@@ -159,10 +159,10 @@ const char* SegMap595Class::get_byte_bin_notation_as_str(unsigned char byte_to_w
         return nullptr;
     }
 
-    // Standard (since GCC 4.3 or C++14) binary number notation prefix "0b" (as in "0bXXXXXXXX").
+    // Standard (since GCC 4.3 or C++14) binary number notation prefix "0b".
     constexpr size_t bin_notation_prefix_len = 2;
 
-    // Binary notation length: prefix + 8 bits.
+    // Binary notation length for a byte: prefix + 8 bits.
     constexpr size_t notation_len = bin_notation_prefix_len + SEGMAP595_SEG_NUM;
 
     static char buf[notation_len + 1];  // Plus one byte for null terminator.
