d29f634fe61383efca93f82d69a3f23c5c06cbe7
ErlingSigurdson <efimov-d-v@yandex.ru>
2025-11-18 03:02:42 +0300

    Fix bug in display type validation sequence.

--- src/SegMap595.cpp
+++ src/SegMap595.cpp
@@ -278,7 +278,7 @@ int32_t SegMap595Class::read_map_str()
 
 int32_t SegMap595Class::map_bytes(DisplayType display_common_pin)
 {
-    if (_display_common_pin != SegMap595CommonCathode && _display_common_pin != SegMap595CommonAnode) {
+    if (display_common_pin != SegMap595CommonCathode && display_common_pin != SegMap595CommonAnode) {
         return SEGMAP595_STATUS_ERR_INVALID_DISPLAY_TYPE;
     } else {
         _display_common_pin = display_common_pin;
